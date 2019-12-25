018d62bb4701dc6176e99e66a2e11efc81edd98d
Evan Kaplan <evan.kaplan@annkissam.com>
2019-12-24 19:36:07 -0500

    Add basic departure display.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'departures/index'
+  resources :departures, only: :index
   root 'departures#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- spec/acceptance/departures_spec.rb
+++ spec/acceptance/departures_spec.rb
@@ -1,14 +1,19 @@
 require 'rails_helper'
 
-RSpec.describe 'Departures' do
+# TODO: RSpec.feature is giving access to view and path helpers, but this is
+# not the conventional usage of it.
+RSpec.feature 'Departures' do
   describe '#index' do
     it 'displays departures' do
-      departure = FactoryBot.create(:departure)
+      departure = FactoryBot.create(:departure, basic_departure_params)
 
-      visit root_path # TODO: change to departrues path
+      visit departures_path
 
-      # TODO: update paramters tested against.
-      expect(page).to have_content departure.time
+      # TODO: Too many expectations. Group or break up.
+      expect(page).to have_content departure.destination
+      expect(page).to have_content departure.train_id
+      expect(page).to have_content departure.track_id
+      expect(page).to have_content departure.status
     end
 
     xit 'sorts departures by time'
@@ -17,11 +22,20 @@ RSpec.describe 'Departures' do
 
     xit 'pulls data from the API adapter'
 
-    # Bells and whistles
     xit 'separates north/south station departures'
 
+    # Bells and whistles
     xit 'updates periodically'
 
-    xit 'updates without refreshing' #can we test this reasonably?
+    xit 'updates without refreshing' # can we test this reasonably?
+  end
+
+  def basic_departure_params
+    {
+      destination: 'Foxboro',
+      train_id: 1234,
+      track_id: 5678,
+      status: 'On Time'
+    }
   end
 end
